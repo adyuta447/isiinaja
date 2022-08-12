6908de5f9114f1794adb8bb4302afbb1b3e2fdcd
K0oceng <yutaagra@gmail.com>
2022-08-12 21:42:29 +0700

    asdasdasd

--- pages/detail/index.tsx
+++ pages/detail/index.tsx
@@ -1,4 +1,5 @@
 import TopUpForm from '../../components/organisms/TopUpForm';
+import TopUpItem from '../../components/organisms/TopUpItem';
 
 export default function Detail() {
   return (
@@ -10,31 +11,10 @@ export default function Detail() {
         </div>
         <div className="row">
           <div className="col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start">
-            <div className="row align-items-center">
-              <div className="col-md-12 col-4">
-                <img src="/img/Thumbnail-3.png" width="280" height="380" className="img-fluid" alt="" />
-              </div>
-              <div className="col-md-12 col-8 d-md-none d-block">
-                <h2 className="text-xl fw-bold color-palette-1 text-start mb-10">
-                  Mobile Legends:
-                  <br />
-                  The New Battle 2021
-
-                </h2>
-                <p className="text-sm color-palette-2 text-start mb-0">Category: Mobile</p>
-              </div>
-            </div>
+            <TopUpItem type="mobile" />
           </div>
           <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
-            <div className="pb-50 d-md-block d-none">
-              <h2 className="text-4xl fw-bold color-palette-1 text-start mb-10 mt-10">
-                Mobile Legends:
-                <br />
-                The New Battle 2021
-
-              </h2>
-              <p className="text-lg color-palette-2 mb-0">Category: Mobile</p>
-            </div>
+            <TopUpItem type="desktop" />
             <hr />
             <TopUpForm />
           </div>
